ba1e33b310c4bac32ccb5d3066193fbaffa0416d
Iryna Didkivska <ididkivska@gmail.com>
2023-07-26 13:49:23 +0200

    add blogcard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { BlogCard } from './BlogCard';
+import article from '../assets/article.json';
+console.log(article);
 export const App = () => {
-  return <div>React homework template</div>;
+  return (
+    <div>
+      <BlogCard article={article} />
+    </div>
+  );
 };
---
+++ src/services/formatDate.js
@@ -0,0 +1,5 @@
+import { formatDistanceToNow } from 'date-fns';
+
+export function formatDateToNow(date) {
+  return formatDistanceToNow(new Date(date), { addSuffix: true });
+}
