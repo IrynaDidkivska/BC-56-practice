82b7f1fc10a9dc6eb9d9a4d6f68dbbf5daf7e728
Sergey <sergsmile29@gmail.com>
2023-07-27 15:54:44 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,35 +2,19 @@ import 'modern-normalize';
 import { styled } from 'styled-components';
 import { BlogCard } from './BlogCard/BlogCard';
 import article from '../assets/article.json';
-import { TodoListFn } from './TodoList';
-
-
 
 console.log(article);
 export const App = () => {
-  const bookList = [
-    { id: 1, price: 5, name: 'FairTale' },
-    { id: 2, price: 10, name: 'Science Fiction' },
-  ];
-
-  const Films = [
-    { id: 1, price: 5, name: 'Film1' },
-    { id: 2, price: 10, name: 'Film2' },
-  ];
-
   return (
-    <Container >
-      <BlogCard article={ article} />
-
+    <Container>
+      <BlogCard article={article} />
     </Container>
   );
 };
-
 
 const Container = styled.div`
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-
-`
+`;
