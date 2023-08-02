d190691061e013a4131050e6e1b4d8915d3305e1
Sergey <sergsmile29@gmail.com>
2023-08-02 22:14:24 +0300

    normalize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-
+import 'modern-normalize';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
