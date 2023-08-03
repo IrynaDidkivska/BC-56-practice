98919f129aff85956329cb4948e0b1d86905a04b
Sergey <sergsmile29@gmail.com>
2023-08-04 01:18:55 +0300

    init

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 import 'modern-normalize';
 import { ThemeProvider } from 'styled-components';
+
+import { App } from 'components/App';
 import { theme } from 'styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
