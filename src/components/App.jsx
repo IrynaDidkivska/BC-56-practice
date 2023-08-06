610f8f647d62bc5851cbb459904ba83548c8b56b
Sergey <sergsmile29@gmail.com>
2023-08-07 01:52:34 +0300

    defaults

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,6 @@
-import React, { Component } from 'react';
-import { ImageGallery } from './image-gallery/ImageGallery';
-import { fetchImg } from 'service/api';
-import { SearchForm } from './SerchForm/SerchForm';
-
-export class App extends Component {
-  state = {
-    loading: false,
-    error: '',
-    images: [],
-    page: 1,
-    per_page: 6,
-    total_result: null,
-    query: 'tree',
-  };
-
-  async componentDidMount() {
-    const { per_page } = this.state;
-    try {
-      const { photos, total_result } = await fetchImg({
-        per_page,
-      });
-
-      this.setState({ images: photos, total_result });
-    } catch (error) {}
-  }
-
-  handleSetSearch = query => {
-    this.setState({ query });
-  };
-
-  render() {
-    return (
-      <div>
-        <SearchForm onSetSearch={this.handleSetSearch} />
-        <ImageGallery images={this.state.images} />
-      </div>
-    );
-  }
-}
+import React from 'react';
+//axios.defaults.baseURL = 'https://restcountries.com/v3.1';
+//https://restcountries.com/#endpoints
+export const App = () => {
+  return <div>App</div>;
+};
